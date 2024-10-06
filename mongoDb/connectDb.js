cbc547de80d2811ef12be671de44b7fd05daf2f5
thakiyudheen <thakeyudheen525@gmail.com>
2024-10-06 15:23:50 +0530

    first

--- mongoDb/connectDb.js
+++ mongoDb/connectDb.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config()
-
+const dotenv = require('dotenv').config();
 
 const connectDb = () => {
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => {
+            console.log('Database connected successfully!');
+        })
+        .catch((error) => {
+            console.error('Database connection error:', error);
+        });
+};
 
-    mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
-        console.log('database connected successfully !');
-
-    })
-
-}
-
-module.exports = {connectDb}
+module.exports = { connectDb };
